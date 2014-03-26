81337a513174a6ba2ec2f4ca4c41871c13654786
Tobias Perelstein <tobias.perelstein@gmail.com>
2014-03-26 01:56:35 -0400

    sucessfully parsing text file for tokens

---
+++ parse.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "uthash.h"
+
+int parse_file(char *);
+
+int parse_file(char *file_name){
+    FILE *fp = fopen(file_name, "r");
+    size_t sizeof_line = 0;
+    char* buffer = NULL;
+    char * token = NULL;
+    char * delim = " \n";
+    ssize_t line_length = 0;
+    /*Taking the next line from fp, allocating space for it in buffer and returns
+     * the length of the line
+     */
+    while ((line_length = getline(&buffer, &sizeof_line, fp)) > 0) {
+        token = strtok(buffer,delim);
+        if(token == NULL){  //case where there is no token
+            continue;       //go to next line
+        }else if(strcmp(token, "<list>") == 0){ //case where it's the first line with key
+            token = strtok(NULL,delim);         //token should now be the key
+            printf("%s\n", token);              //this is where we create node
+        }else if(strcmp(token, "</list>") == 0){ //case where it's the end of that key
+            token = NULL;
+            continue;                   //this hash thing is done, add node to hash here
+        }else{                          //case for file names/counts
+            while(token != NULL && strlen(token) > 0){
+                if(!isdigit(token[0])){
+                    printf("%s\n", token); //add to linked list
+                }
+                token = strtok(NULL, delim);
+            }
+        }
+    }
+    if(buffer) free(buffer);
+    fclose(fp);
+    return 1;
+}
+
+int main(int argc, char **argv){
+    if(argc != 2){
+        printf("incorrect number of arguments\n");
+        return 0;
+    }
+    parse_file(argv[1]);
+    return 0;
+
+}
+
+
+
